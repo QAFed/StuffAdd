c8e2186c4deaa66f28c29f1634e7a619f81e3293
Fedor <idolenkov@gmail.com>
2024-11-15 18:26:04 +0300

    user n division page

---
+++ pages/divdsion_page.py
@@ -0,0 +1,35 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class DivisPage(BasePage):
+    xp_bkmark_didvs = By.XPATH, '//div[text()="Подразделения"]'
+    xp_button_add_division = By.XPATH, '//div[@style="height: 50%;"]/div/div/div/span/span[text()="Добавить..."]'
+    xp_button_add_job = By.XPATH, '//div[not(@style="height: 50%;")]/div/div/div/span/span[text()="Добавить..."]'
+
+    xp_input_code_divis = By.XPATH, '//div[contains(@style, "top: 6px;")]/input'
+    xp_input_nane_divis = By.XPATH, '//div[contains(@style, "top: 36px;")]/input'
+    xp_input_descr_divis = By.XPATH, '//div[contains(@style, "top: 66px;")]/input'
+    xp_button_save_div = By.XPATH, '//span[text()="Сохранить"]'
+
+    xp_input_nane_job = By.XPATH, '//div[contains(@style, "top: 0px;")]/input'
+    xp_input_descr_job = By.XPATH, '//div[contains(@style, "top: 30px;")]/input'
+    xp_input_count_job = By.XPATH, '//div[contains(@style, "top: 60px;")]/input'
+    xp_button_save_job = By.XPATH, '//span[text()="Сохранить"]'
+
+    def add_devision(self, code, name, desc):
+        self.ac_click_element(self.xp_button_add_division)
+        self.fill_field(self.xp_input_code_divis, code)
+        self.fill_field(self.xp_input_nane_divis, name)
+        self.fill_field(self.xp_input_descr_divis, desc)
+        self.ac_click_element(self.xp_button_save_div)
+
+    def add_job(self, name, desc, count):
+        self.ac_click_element(self.xp_button_add_job)
+        self.fill_field(self.xp_input_nane_job, name)
+        self.fill_field(self.xp_input_descr_job, desc)
+        self.fill_field(self.xp_input_count_job, count)
+        self.ac_click_element(self.xp_button_save_job)
+
+
+
+
---
+++ pages/user_page.py
@@ -0,0 +1,47 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class UserPage(BasePage):
+    xp_bkmark_users = By.XPATH, '//div[text()="Пользователи"]'
+    xp_button_add_users = By.XPATH, '//div[contains(@style, "-6px")]//span[text()="Добавить..."]'
+    xp_input_family_user = By.XPATH, '//div[contains(@style,"top: 42px")]/input'
+    xp_input_name_user = By.XPATH, '//div[contains(@style,"top: 72px")]/input'
+    xp_input_second_name_user = By.XPATH, '//div[contains(@style,"top: 102px")]/input'
+    xp_input_birth_day_user = By.XPATH, '//div[contains(@style,"top: 138px")]//input'
+    xp_button_save = By.XPATH, '//span[text()="Сохранить"]'
+
+    xp_button_add_contact = By.XPATH, '//div[contains(@style, "-7px")]//span[text()="Добавить..."]'
+    xp_select_type = By.XPATH, '//select'
+    xp_select_mob_tel = By.XPATH, '//select/option[text(),"Мобильный телефон"]'
+    xp_input_tel = By.XPATH, '//select/../../../div/input'
+
+    xp_button_add_job = By.XPATH, '//div[not(contains(@style, "-"))]/div/div/div/div/div/div/span/span[text()="Добавить..."]'
+
+    def click_bm_users(self):
+        self.ac_click_element(self.xp_bkmark_users)
+    def add_user(self, user_list):
+        self.ac_click_element(self.xp_bkmark_users)
+        self.fill_field(self.xp_input_family_user, user_list[0])
+        self.fill_field(self.xp_input_name_user, user_list[1])
+        self.fill_field(self.xp_input_second_name_user, user_list[2])
+        self.fill_field(self.xp_input_birth_day_user, user_list[3])
+        self.ac_click_element(self.xp_button_save)
+
+    def add_tell(self, user_list):
+        self.ac_click_element(By.XPATH, f'//tr/td/div[text()="{user_list[0]} {user_list[1]} {user_list[2]}"]')
+        self.ac_click_element(self.xp_button_add_contact)
+        self.ac_click_element(self.xp_select_type)
+        self.ac_click_element(self.xp_select_mob_tel)
+        self.fill_field(self.xp_input_tel, user_list[4])
+        self.ac_click_element(self.xp_button_save)
+
+    def add_job(self, user_list):
+        self.ac_click_element(By.XPATH, f'//tr/td/div[text()="{user_list[0]} {user_list[1]} {user_list[2]}"]')
+        self.ac_click_element(self.xp_button_add_job)
+        self.ac_click_element(By.XPATH, f'//span[text()="{user_list[5]}"]')
+        self.ac_click_element(self.xp_select_type)
+        self.ac_click_element(By.XPATH, f'//select/option[text(),"[{user_list[6]}"]')
+        self.ac_click_element(self.xp_button_save)
+
+
+
